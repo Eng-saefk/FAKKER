ca38a43ebe0d48095ccfcbdd462a19d68ac4abe2
saef aldeen <sal74@smail.ucas.edu.ps>
2026-02-06 00:21:06 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // أضف هذا السطر هنا لتخطي حماية الـ CSRF
+        $middleware->validateCsrfTokens(except: [
+            '*', 
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
